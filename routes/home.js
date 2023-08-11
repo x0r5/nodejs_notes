e26195c59fceb41e220b271f5029f74cf03eedfe
Sebestyen Soma <obivan05@gmail.com>
2023-08-11 17:19:46 +0200

    basic iteration with pug

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,19 @@ const router = express.Router();
 
 router.get('/', function(req, res, next){
     //res.send("Home router test");
-    res.render('home', { title: 'Home'});
+    let noteList = getLatestNotes();
+    res.render('home', { title: 'Home', noteList});
 })
+
+
+function getLatestNotes(){
+    //without db
+    let noteList = [
+        {Title: "First", CreatedDate: new Date(2023,8,10), Text: "Hello there, General Kenobi."},
+        {Title: "Second", CreatedDate: new Date(2023,8,11), Text: "Hello there, General Kenobi 2."}
+    ];
+
+    return noteList;
+}
 
 module.exports = router;
